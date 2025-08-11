1b685e2b8790431241684b511f30df0f0499f859
gyanendra-kumar-dps <gyanendra.kumar1210@gmail.com>
2025-08-11 21:47:32 +0530

    first commit

--- one.py
+++ one.py
@@ -1,1 +1,9 @@
-print('33033')
+inp=input('Enter your name:')
+print(f'Hello {inp}, Nice to meet you.')
+a=input('How are you feeling today. Good/Bad:')
+if a=='Good':
+    print(f'{inp} I am glad you are feeling good')
+elif a=='Bad':
+    print(f'I am sorry to hear that {inp}')
+else:
+    print('I could not understand')
