94fd08a23dfeb4fd2b62fb327d8520bd26997cd3
gyanendra-kumar-dps <gyanendra.kumar1210@gmail.com>
2025-09-22 21:41:05 +0530

    first commit

---
+++ edge_detection.py
@@ -0,0 +1,14 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+image=cv2.imread('tree.jpg')
+def show_img(image,title):
+    plt.figure(figsize=(8,8))
+    plt.imshow(cv2.cvtColor(image,cv2.COLOR_BGR2RGB))
+    plt.title(title)
+plt.imshow(cv2.bitwise_or(cv2.Sobel(cv2.cvtColor(image,cv2.COLOR_BGR2GRAY),cv2.CV_64F,0,1,ksize=3).astype(np.uint8),cv2.Sobel(cv2.cvtColor(image,cv2.COLOR_BGR2GRAY),cv2.CV_64F,1,0,ksize=3).astype(np.uint8)))
+plt.imshow(cv2.Canny(image,100,300))
+plt.imshow(cv2.Laplacian(image,cv2.CV_64F))
+plt.imshow(cv2.GaussianBlur(cv2.cvtColor(image,cv2.COLOR_BGR2RGB),(11,13),0))
+plt.imshow(cv2.medianBlur(cv2.cvtColor(image,cv2.COLOR_BGR2RGB),13))
+plt.show()
