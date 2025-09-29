6fe49870f5edeef7c5ab4631ddb907fe2a17f2cc
gyanendra-kumar-dps <gyanendra.kumar1210@gmail.com>
2025-09-29 21:41:04 +0530

    first commit

--- face_detect.py
+++ face_detect.py
@@ -5,6 +5,6 @@ r,frame=cap.read()
 gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 face=face_detector.detectMultiScale(gray,1.1,5,minSize=(40,40))
 for (w,x,y,z) in face:
-    cv2.rectangle(gray,(w,x),(w+x,y+z),(255,0,255),2)
+    cv2.rectangle(gray,(w,x),(w+y,x+z),(255,0,255),2)
 cv2.imshow('win',gray)
 cv2.waitKey(0)
