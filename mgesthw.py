b1d979108befae1915e7f69c3995ef0cd0ce0cd0
gyanendra-kumar-dps <gyanendra.kumar1210@gmail.com>
2025-10-29 16:16:58 +0530

    first commit

---
+++ mgesthw.py
@@ -0,0 +1,15 @@
+import mediapipe as mps
+import cv2
+import os
+cam=cv2.VideoCapture(0)
+hand=mps.solutions.hands
+hand_obj=hand.Hands(min_detection_confidence=0.7,min_tracking_confidence=0.7)
+draw=mps.solutions.drawing_utils
+while True:
+    _,ret=cam.read()
+    result=hand_obj.process(cv2.cvtColor(ret, cv2.COLOR_BGR2RGB))
+    if result.multi_hand_landmarks:
+        for i,handlm in enumerate(result.multi_hand_landmarks):  
+            draw.draw_landmarks(ret,handlm,hand.HAND_CONNECTIONS)
+    cv2.imshow('',ret)
+    cv2.waitKey(1)
