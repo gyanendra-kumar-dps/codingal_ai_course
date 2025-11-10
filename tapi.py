cc95e4bc7c0697a40a6e382f4704de7c9f354bc0
gyanendra-kumar-dps <gyanendra.kumar1210@gmail.com>
2025-11-10 21:47:53 +0530

    first commit

---
+++ tapi.py
@@ -0,0 +1,42 @@
+import requests
+import random
+import html
+url='https://opentdb.com/api.php?amount=5&type=multiple&category=9'
+def get_data():
+    data=requests.get(url)
+    json_data=data.json()
+    if data.status_code==200:
+        return json_data['results']
+def quiz():
+    data=get_data()
+    options=[]
+    correct=[]
+    questions=[]
+    for idx,i in enumerate(data):
+        options.append(html.unescape(i['incorrect_answers']))
+        correct.append(html.unescape(i['correct_answer']))
+        options[idx].append(html.unescape(i['correct_answer']))
+        questions.append(html.unescape(i['question']))
+    count=0
+    score=0
+    while True:
+        if count==5:
+            break
+        print(f"Question:- {questions[count]}")
+        for idx,i in enumerate(options[count]):
+            print(f'{idx+1}:- {html.unescape(i)}')
+        inp=input('enter a option (1-4)')
+        try:
+            if options[count][int(idx-1)]==correct[count]:
+                score+=10
+                print('correct')
+            else:
+                print(options[count][int(idx-1)],correct[count])
+                print('wrong')
+                print(f'this is correct {correct[count]}')
+        except:
+            pass
+        count+=1
+    print(score)
+    print(f'percentage:-{score/count*100:.1f}')
+quiz()
