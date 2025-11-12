27360a0309a1fedac3c3c4d544b560a7ceb2d2f0
gyanendra-kumar-dps <gyanendra.kumar1210@gmail.com>
2025-11-12 21:14:58 +0530

    first commit

--- rfapi.py
+++ rfapi.py
@@ -1,2 +1,15 @@
 import requests
-res=requests.get('')
+
+def get_random_facts():
+    res=requests.get('https://uselessfacts.jsph.pl/random.json?language=en')
+    status_code=res.status_code
+    if status_code==200:
+        data=res.json()
+        return data['text']
+    else:
+        return f"error {status_code}"
+while True:
+    inp=input("Press enter to get a random fact...")
+    if inp=='q' or inp=='Q':
+        exit()
+    print(get_random_facts())
