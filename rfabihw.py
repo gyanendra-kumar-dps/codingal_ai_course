3063fe87d90e1c4f25fc0ce3a661d060cb22613c
gyanendra-kumar-dps <gyanendra.kumar1210@gmail.com>
2025-11-14 12:09:42 +0530

    first commit

--- rfabihw.py
+++ rfabihw.py
@@ -1,2 +1,18 @@
 import requests
-res=requests.get('https://uselessfacts.jsph.pl/random.json?language=en')
+def get_random_facts(x):
+    res=requests.get(f'https://uselessfacts.jsph.pl/{x}.json?language=en')
+    status_code=res.status_code
+    if status_code==200:
+        data=res.json()
+        return data['text']
+    else:
+        return f"error {status_code}"
+typeof=input("Enter which fact you want(today,random):")
+if typeof.lower()!='today' and typeof.lower()!='random':
+    print('Please type random or today only')
+    exit()
+while True:
+    inp=input("Press enter to get a random fact...")
+    if inp=='q' or inp=='Q':
+        exit()
+    print(get_random_facts(typeof.lower()))
