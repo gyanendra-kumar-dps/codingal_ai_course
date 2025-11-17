cf7b7953f87833d2431f03f8209578d18bde8f49
gyanendra-kumar-dps <gyanendra.kumar1210@gmail.com>
2025-11-17 21:26:34 +0530

    first commit

---
+++ nlp.py
@@ -0,0 +1,13 @@
+from config import api_key
+import requests
+url="\
+https://router.huggingface.co/hf-inference/models/distilbert/distilbert-base-uncased-finetuned-sst-2-english"
+headers={'Authorization':f'Bearer {api_key}'}
+def get_data(text):
+    res=requests.post(url,headers=headers,json={'inputs':text})
+    return res.json()
+while True:
+    inp=input("Enter a sentence for analysis:")
+    data=get_data(inp)
+    print(f"{data[0][0]['label']}:- {data[0][0]['score']*100:.4f}%")
+    print(f"{data[0][1]['label']}:- {get_data(inp)[0][1]['score']*100:.4f}%")
