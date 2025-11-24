8dc6b4b206fb86ad9695f09ddc61f101dc0da034
gyanendra-kumar-dps <gyanendra.kumar1210@gmail.com>
2025-11-24 21:41:48 +0530

    first commit

---
+++ summaryai.py
@@ -0,0 +1,23 @@
+from colorama import init,Fore
+import requests
+from config import api_key
+def query(payload,modelname):
+    header={"Authorization":f"Bearer {api_key}"}
+    res=requests.post(f"https://router.huggingface.co/hf-inference/models/{modelname}",headers=header,json=payload)
+    return res.json()
+def summary_of_text(text,minl,maxl,modelname="google/pegasus-xsum"):
+    payload={
+        "inputs":text,
+        "parameters":{"min_length":minl,"max_length":maxl}
+    }
+    print(Fore.BLUE+f"Performing summary using ai model {modelname}")
+    res=query(payload,modelname)
+    print(Fore.BLUE+"Your summary creation is done")
+    return res
+if __name__=="__main__":
+    name=input("Hey what is your name:")
+    mdl=input("What model would you like to use(google/pegasus-xsum,facebook/bart):")
+    mil=input("What min len you want for summary")
+    mal=input("What max len you want for summary")
+    text=input("What is the text")
+    print(summary_of_text(text,mil,mal,mdl))
