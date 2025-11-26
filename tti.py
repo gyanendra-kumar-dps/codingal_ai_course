c4776e897f1201955599de5ff09f1f1e02e6ddc5
gyanendra-kumar-dps <gyanendra.kumar1210@gmail.com>
2025-11-26 21:36:10 +0530

    first commit

--- tti.py
+++ tti.py
@@ -2,15 +2,16 @@ import requests
 from PIL import Image
 from io import BytesIO
 from config import api_key
+from time import time
 url="https://api-interface.huggingface.co/models/stabilityai/stable-diffusion-3-medium-diffusers"
-def gen_image(inp):
-    header={"Authorization":f"Bearer {api_key}"}
-    input={"inputs":inp}
-    res=requests.post(url,headers=header,json=input,timeout=30)
-    if 'image' in res.headers.get("Content-Type"):
-        img=Image.open(BytesIO(res.content))
-        return img
-
-while True:
-    inputs=input("Enter your prompt:")
-    gen_image(inputs)
+def gen_image():
+    while True:
+        inp=input("Enter your prompt:")
+        header={"Authorization":f"Bearer {api_key}","Accept":"image/png"}
+        inpu={"inputs":inp}
+        res=requests.post(url,headers=header,json=inpu,timeout=30)
+        if 'image' in res.headers.get("Content-Type"):
+            img=Image.open(BytesIO(res.content))
+            img.save(f"file{time()}",)
+        
+gen_image()
